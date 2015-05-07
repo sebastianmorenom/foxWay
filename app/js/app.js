1e9dfa78df2fe7652e66be87a512b859d8a46b11
Sebastian Moreno <zebaxtian@hotmail.com>
2015-05-07 14:16:27 -0500

    base de la pagina

---
+++ app/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var angularModule = angular.module('foxWay', ['ngRoute']);
+
+/*angularModule.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider
+        .when('/1', {
+            templateUrl: '/views/1.html',
+            requireLogin: false
+        })
+        .when('/2', {
+            templateUrl: '/views/2.html',
+            requireLogin: false
+        })
+        .otherwise({
+            redirectTo: '/1'
+        });
+    
+}]);*/
