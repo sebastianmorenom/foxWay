a4fde894c9bc1b5ee8a5be7b5f86d5d926df6085
Sebastian Moreno <zebaxtian@hotmail.com>
2015-05-12 10:59:22 -0500

    app.js update, routProvider

--- app/js/app.js
+++ app/js/app.js
@@ -5,16 +5,30 @@ var angularModule = angular.module('foxWay', ['ngRoute']);
 /*angularModule.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider
-        .when('/1', {
+        .when('/home', {
             templateUrl: '/views/1.html',
-            requireLogin: false
         })
-        .when('/2', {
+        .when('/page1', {
             templateUrl: '/views/2.html',
-            requireLogin: false
         })
         .otherwise({
-            redirectTo: '/1'
+            redirectTo: '/home'
         });
     
 }]);*/
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
