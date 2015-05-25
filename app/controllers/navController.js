19451966eba08937de9a647c8afb1a0fc39b837d
Sebastian Moreno <zebaxtian@hotmail.com>
2015-05-25 18:26:34 -0500

    nav, no ngRoute
    navbar, adelanto carousel

---
+++ app/controllers/navController.js
@@ -0,0 +1,7 @@
+
+foxWay.controller('navController', ['$scope', function($scope){
+    
+    $scope.tab=1;
+    console.log($scope.tab);
+    
+}]);
--- app/js/app.js
+++ app/js/app.js
@@ -1,34 +1,16 @@
 'use strict';
 
-var angularModule = angular.module('foxWay', ['ngRoute']);
+var foxWay = angular.module('foxWay', ['ngRoute']);
 
-/*angularModule.config(['$routeProvider', function($routeProvider) {
+
+/*foxWay.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider
-        .when('/home', {
-            templateUrl: '/views/home.html',
-        })
-        .when('/page1', {
-            templateUrl: '/views/page1.html',
+        .when('#', {
+            templateUrl: 'views/home.html'
         })
         .otherwise({
-            redirectTo: '/home'
+            redirectTo: '#'
         });
     
 }]);*/
-
-phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
